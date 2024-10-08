f2b1c15a08d9e74899cd5fbbfc41c4762b93ec10
danieliank <daxter1835@gmail.com>
2024-10-08 13:00:33 +0700

    Initial Commit

---
+++ ianlearncloud/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ianlearncloud
+//
+//  Created by Daniel Ian on 08/10/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
